1234d660b17da89ffa1d8f533702967e7592e365
Justin <jdlow2@gmail.com>
2019-02-22 21:29:18 -0500

    working on date formatting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import Landing from './components/Landing';
-import CssBaseline from '@material-ui/core/CssBaseline'
+import React from "react";
+import Landing from "./components/Landing";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <CssBaseline />
-        <Landing />
-      </div>
-    );
-  }
+function App() {
+  const theme = {
+    primary: "red"
+  };
+
+  return (
+    <div className="App">
+      <CssBaseline />
+      <Landing theme={theme} />
+    </div>
+  );
 }
 
 export default App;
